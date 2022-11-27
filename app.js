535b106d4e3f524a5958ecf678cc4d3b940387fc
PettersonWilliam <petterson309@gmail.com>
2022-11-27 02:08:12 -0300

    feat(insering-list(matc.participants))

---
+++ app.js
@@ -0,0 +1,43 @@
+//importando o angular definindo ela com o angular.module e definindo tbm os pacotes que irao seer usados como ui.router.
+const myApp = angular.module("jogaFacilApp", ["ui.router"]); // ui.router -- BIBLIOTECA - LIB
+
+myApp.config(function ($stateProvider, $httpProvider) {
+    $httpProvider.interceptors.push('BearerTokenInterceptor');
+
+    $stateProvider.state({
+        name: "login", // nome do estado, estando ativo
+        url: "", // nesta url sendo ativa
+        templateUrl: "views/login.html", // o html que vai ser renderizado
+        controller: "LoginCtrl", // ultilizando o controller , onde fica toda nossa logica
+        onEnter: checkRedirect
+    });
+
+    $stateProvider.state({
+        name: "home",
+        url: "/home",
+        templateUrl: "views/home.html",
+        controller: "HomeCtrl",
+        onEnter: isAuthorized
+    });
+});
+
+const isAuthorized = ($state, $rootScope) => {
+    const isLogged = localStorage.getItem("token");
+
+    if (!isLogged) {
+      $state.go("login");
+      return;
+    }
+
+    $rootScope.isLogged = true;
+};
+
+const checkRedirect = ($state, $rootScope) => {
+    const isLogged = localStorage.getItem("token");
+
+    if (!isLogged) {
+        return;
+    }
+
+    $state.go("home");
+}
